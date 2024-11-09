d09e1292c13a5ba3559c7d417b91bbe77889fa4b
JoseVitor0 <144168385+JoseVitor0@users.noreply.github.com>
2024-11-09 12:06:44 -0300

    instruções finais

--- assembly.c
+++ assembly.c
@@ -2,8 +2,10 @@
 #include <string.h>
 
 #define NUM_INSTRUCOES 100
+#define MEM_SIZE 1024  // Define o tamanho da memória para simulação
 
 int PC = 0;
+int memoria[MEM_SIZE] = {0}; 
 
 typedef struct {
     char label[10];
@@ -37,14 +39,16 @@ void instrucoesBranch(char *tipo, int reg1, int reg2, char *label, int *registra
     }
 
     if ((strcmp(tipo, "bgt") == 0 && registradores[reg1] > registradores[reg2]) ||
-        (strcmp(tipo, "bge") == 0 && registradores[reg1] >= registradores[reg2])) {
-        PC = endereco_label;
-        printf("PC após BG: %d\n", PC);
+        (strcmp(tipo, "beq") == 0 && registradores[reg1] == registradores[reg2]) ||
+        (strcmp(tipo, "ble") == 0 && registradores[reg1] <= registradores[reg2])){
+            PC = endereco_label;
+            printf("PC apos comparacao: %d\n", PC);
     } else {
+        printf("Comparacao nao satisfeita.\n");
         PC++;
     }
 
-    printf("PC após BG: %d\n", PC);
+    printf("PC apos comparacao: %d\n", PC);
 }
 
 
@@ -52,42 +56,25 @@ void instrucoesOperacoes(char *comando, int reg0, int reg1, int reg2, int *regis
     //add $0, $1, $2
     //Instruções do tipo R: Add, Sub, And
 
-    char add[4] = "add";
-    char sub[4] = "sub";
-    char addu[4] = "addu";
-    char addi[4] = "addi";
-    
-    if(strcmp(comando, add) == 0){
+    if(strcmp(comando, "add") == 0){
         registradores[reg0] = registradores[reg1] + registradores[reg2];
 
         printf("\nRegistradores modificados: \n");
         printf("$%d = %d, $%d = %d, $%d = %d", reg0, registradores[reg0], reg1, registradores[reg1], reg2, registradores[reg2]);
 
-    } else if (strcmp(comando, sub) == 0){
+    } else if (strcmp(comando, "sub") == 0){
 
         registradores[reg0] = registradores[reg1] - registradores[reg2];
         printf("Registradores modificados: \n");
         printf("$%d = %d, $%d = %d, $%d = %d", reg0, registradores[reg0], reg1, registradores[reg1], reg2, registradores[reg2]);
 
-    } else if (strcmp(comando, addu) == 0){
+    } else if (strcmp(comando, "mul") == 0){
         
-        int temp1 = 0;
-        int temp2 = 0;
+        registradores[reg0] = registradores[reg1] * registradores[reg2];
+        printf("\nRegistradores modificados: \n");
+        printf("$%d = %d, $%d = %d, $%d = %d", reg0, registradores[reg0], reg1, registradores[reg1], reg2, registradores[reg2]);
 
-        if (registradores[reg1] < 0){
-            temp1 = registradores[reg1] * (-1);
-        } else{
-            temp1 = registradores[reg1];
-        }
-
-        if (registradores[reg2] < 0){
-            temp2 = registradores[reg2] * (-1);
-        }else{
-            temp2 = registradores[reg2];
-        }
-        registradores[reg0] = temp1 + temp2;
-
-    } else if(strcmp(comando, addi)){
+    } else if(strcmp(comando, "addi")){
 
         registradores[reg0] = registradores[reg1] + reg2;
 
@@ -102,20 +89,33 @@ void instrucoesOperacoes(char *comando, int reg0, int reg1, int reg2, int *regis
 
 }
 
+void instrucaoJump(char *label) {
+    int jump_addr = buscarLabel(label);
+    if (jump_addr != -1) {
+        PC = jump_addr;
+        printf("JUMP: PC alterado para %d\n", PC);
+    } else {
+        printf("ERRO! Label %s não encontrada\n", label);
+    }
+}
+
+void instrucaoNeg(int reg1, int reg2, int *registradores){
+    registradores[reg1] = (registradores[reg2] * -1);
+    printf("Registradores modificados: \n");
+    printf("$%d = %d, $%d = %d\n", reg1, registradores[reg1], reg2, registradores[reg2]);
+}
+
+
 int reconhecerComando(char *comando){
 
-    char add[4] = "add";
-    char sub[4] = "sub";
-    char addu[5] = "addu";
-    char addi[5] = "addi";
-
-    char bge[4] = "bge";
-    char bgt[4] = "bgt";
-
-    if (strcmp(comando, add) == 0 || strcmp(comando, sub) == 0 || strcmp(comando, addu) == 0 || strcmp(comando, addi) == 0){
+    if (strcmp(comando, "add") == 0 || strcmp(comando, "sub") == 0 || strcmp(comando, "mul") == 0 || strcmp(comando, "addi") == 0){
         return 1;
-    } else if(strcmp(comando, bge) == 0 || strcmp(comando, bgt) == 0){
+    } else if(strcmp(comando, "beq") == 0 || strcmp(comando, "bgt") == 0  || strcmp(comando, "ble") == 0){
         return 2;
+    } else if (strcmp(comando, "j") == 0) {
+        return 3;
+    } else if (strcmp(comando, "neg") == 0){
+        return 4;
     }
     return 0;
 
@@ -146,9 +146,12 @@ int main()
     char label[10];
     char addi[5] = "addi";
 
-    adicionarLabel("init", 0);
-    adicionarLabel("loop_for", 5);
-    adicionarLabel("fim", 10);
+    adicionarLabel("init", 4);
+    adicionarLabel("if", 24);
+    adicionarLabel("else", 28);
+    adicionarLabel("loop_for", 16);
+    adicionarLabel("fim", 32);
+
 
     //Instruções do tipo R: Add, Sub, And
     //Instruções do tipo I: Addi, Bgt, Bge,
@@ -165,8 +168,8 @@ int main()
 
     do{
         printf("Digite o comando da operacao que deseja realizar");
-        printf("\nOperacoes disponiveis: Add, Sub, Addu, Addi, BGE, BGT\n");
-        printf("Para instruções BGT e BGE, labels disponiveis: 'init', 'loop_for', 'fim'\n");
+        printf("\nOperacoes disponiveis: Add, Sub, Mul, Addi, Beq, Bgt, Ble, J, Neg\n");
+        printf("Para instruções BGT, BEQ, BLE e J, labels disponiveis: 'init', 'loop_for', 'fim'\n");
         scanf("%[^\n]", instrucao); 
         printf("%s\n", instrucao);
 
@@ -236,8 +239,36 @@ int main()
                     instrucoesBranch(comando, registradores[atoi(reg1 + 1)], registradores[atoi(reg2 + 1)], label, registradores);
                 }
             break;
+
+            case 3:
+                if (token != NULL) { 
+                    strcpy(label, token); 
+                }
+                instrucaoJump(label);
+                break;
+            
+            case 4:
+
+                if (token != NULL) {
+                    strcpy(reg1, token); 
+                    token = strtok(NULL, ", "); 
+                }
+                if (token != NULL) {
+                    strcpy(reg2, token);
+                    token = strtok(NULL, ", "); 
+                }
+
+                if (!validar_registrador(reg1) || !validar_registrador(reg2)) {
+                        printf("Erro: Registradores não reconhecidos ou fora do formato esperado ($0 a $31).\n");
+                } else{
+                    instrucaoNeg(registradores[atoi(reg1 + 1)], registradores[atoi(reg2 + 1)], registradores);
+                }
+                
+                break;
+
         
         default:
+            printf("Erro: Comando não reconhecido\n");
             break;
         }
 
